e5ffa3e94e346411ffc67432ff5b01fe648062ec
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-05-18 16:02:56 -0700

    Generate the individual squares, and make them clickable

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const gameBoard = (() => {
   const buildBoard = () => {
+    // Board
     const body = document.querySelector('body');
     const board = document.createElement('div');
     board.setAttribute('id', 'board');
     body.appendChild(board);
+
+    // Squares
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.textContent = 'square';
+        square.addEventListener('click', () => {
+          square.textContent = 'hat';
+        });
+        board.appendChild(square);
+      }
+    }
   };
   
   return { buildBoard };
